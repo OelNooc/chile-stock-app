3af291027359bbc27a485095145904cd650902c3
OelNooc <leo.catejoa@gmail.com>
2025-09-30 18:37:17 -0300

    fix: update models to match given jsons structure

--- src/app/core/models/instrument.model.ts
+++ src/app/core/models/instrument.model.ts
@@ -1,22 +1,79 @@
-export interface Instrument {
-    symbol: string;
+export interface ConstituentInstrument {
+    codeInstrument: string;
     name: string;
+    shortName: string;
+    pctDay: number;
+    pct30D: number;
+    pctCY: number;
+    pct1Y: number;
     lastPrice: number;
-    change: number;
-    changePercent: number;
-    volume: number;
-    marketCap: number;
-    high: number;
-    low: number;
-    open: number;
-    previousClose: number;
+    datetimeLastPrice: string;
+    volumeMoney: number;
+    accumulatedVolumeMoney: number;
+    tend: 'up' | 'down' | 'same';
+    performanceAbsolute: number;
+    performanceRelative: number;
 }
 
-export interface InstrumentDetail extends Instrument {
-    bid: number;
-    ask: number;
-    dayRange: { min: number; max: number };
-    yearRange: { min: number; max: number };
-    avgVolume: number;
-  }
-  
+export interface ConstituentsResponse {
+    success: boolean;
+    code: number;
+    data: {
+      info: {
+        name: string;
+        shortName: string;
+        countryName: string;
+        codeInstrument: string;
+      };
+      constituents: ConstituentInstrument[];
+    };
+}
+
+export interface InstrumentDetail {
+    info: {
+      name: string;
+      shortName: string;
+      countryName: string;
+      currencyName: string;
+      currencySymbol: string;
+      codeInstrument: string;
+      marketName: string;
+      hourOpen: string;
+      hourClose: string;
+      trading: boolean;
+      exchangeRate: number;
+    };
+    price: {
+      lastPrice: number;
+      datetimeLastPrice: string;
+      openPrice: number;
+      closePrice: number;
+      datetimeClosePrice: string;
+      performanceAbsolute: number;
+      performanceRelative: number;
+      bid: number;
+      bidVolume: number;
+      bidDatetime: string;
+      ask: number;
+      askVolume: number;
+      askDatetime: string;
+      volumeMoney: number;
+      accumulatedVolumeMoney: number;
+      volumeInstrument: number;
+      accumulatedVolumeInstrument: number;
+      tend: 'up' | 'down' | 'same';
+      maxDay: number;
+      minDay: number;
+      min52W: number;
+      max52W: number;
+      pct30D: number;
+      pctRelW52: number;
+      pctRelCY: number;
+    };
+}
+
+export interface InstrumentDetailResponse {
+    success: boolean;
+    code: number;
+    data: InstrumentDetail;
+}
